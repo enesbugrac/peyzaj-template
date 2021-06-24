348e26551c9d0e3158c326b040b7fe101a263355
enesbugrac <enesbc46@gmail.com>
2021-06-24 14:16:46 +0300

    hakkımızda

--- src/components/HizmetTekli.js
+++ src/components/HizmetTekli.js
@@ -1,21 +1,48 @@
 import React from "react";
+import cx from "clsx";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardContent from "@material-ui/core/CardContent";
+import TextInfoContent from "@mui-treasury/components/content/textInfo";
+import { useFourThreeCardMediaStyles } from "@mui-treasury/styles/cardMedia/fourThree";
+import { useN04TextInfoContentStyles } from "@mui-treasury/styles/textInfoContent/n04";
+import { useOverShadowStyles } from "@mui-treasury/styles/shadow/over";
 import "./HizmetTekli.css";
-function HizmetTekli({ link }) {
-  return (
-    <div className="hizmettekli">
-      <img className="hizmet_img" alt="" src={link} width="100" />
-      <h4>PROJE YÖNETİM</h4>
-      <p>
-        BETULA olarak tüm peyzaj mimarlığı hizmetlerine hakimiyetimiz , uzun
-        firma geçmişimizden gelen deneyimimiz ve güncel tüm sektör verilerini
-        analiz edebilen kadromuzla peyzaj proje yönetimi ve danışmanlığı ile
-        ilgili çalışmalar gerçekleştirmekteyiz.Peyzaj proje yönetim sistemimizle
-        işveren adına projenin en akılcı ,en doğru fiyata ve tam zamanında
-        uygulanmasını sağlamayı , tüm süreci raporlayarak kontrolü
-        kolaylaştırmayı, uygulama sonunda sürprizsiz sonları garanti ediyoruz.
-      </p>
-    </div>
-  );
-}
+const useStyles = makeStyles(() => ({
+  root: {
+    maxWidth: 460,
+    margin: "auto",
+    borderRadius: 12,
+    padding: 12,
+  },
+  media: {
+    borderRadius: 6,
+  },
+}));
 
-export default HizmetTekli;
+export const MusicCardDemo = React.memo(function MusicCard({ link, proje }) {
+  const styles = useStyles();
+  const mediaStyles = useFourThreeCardMediaStyles();
+  const textCardContentStyles = useN04TextInfoContentStyles();
+  const shadowStyles = useOverShadowStyles({ inactive: true });
+  return (
+    <Card
+      className={cx(styles.root, shadowStyles.root)}
+      style={{ marginBottom: 10 }}
+    >
+      <CardMedia className={cx(styles.media, mediaStyles.root)} image={link} />
+      <CardContent className="title">
+        <TextInfoContent
+          classes={textCardContentStyles}
+          color="#95d5b2"
+          heading={proje}
+          body={
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+          }
+        />
+      </CardContent>
+    </Card>
+  );
+});
+export default MusicCardDemo;
--- src/components/pages/Hizmetlerimiz.js
+++ src/components/pages/Hizmetlerimiz.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Navbar from "../Navbar";
 import Footer from "../Footer";
-import HizmetTekli from "../HizmetTekli";
+import MusicCardDemo from "../HizmetTekli";
 
 import "../Urunlerimiz.css";
 import Fade from "react-reveal/Fade";
@@ -12,12 +12,24 @@ function Urunlerimiz() {
       <Navbar />
       <div className="urunlerimizpage">
         <div className="urunlerimizpagewrap">
-          <HizmetTekli link="https://betula.com.tr/themes/peyzaj/img/img-icon/icon-projeyonetim.png" />
-          <HizmetTekli link="https://betula.com.tr/themes/peyzaj/img/img-icon/icon-projeyonetim.png" />
+          <MusicCardDemo
+            proje="PROJE"
+            link="https://images.unsplash.com/photo-1571493322333-94ab5b524af4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1789&q=80"
+          />
+          <MusicCardDemo
+            proje="UYGULAMA"
+            link="https://images.unsplash.com/photo-1419332552192-47d29e59b1e0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=753&q=80"
+          />
         </div>
         <div className="urunlerimizpagewrap">
-          <HizmetTekli link="https://betula.com.tr/themes/peyzaj/img/img-icon/icon-projeyonetim.png" />
-          <HizmetTekli link="https://betula.com.tr/themes/peyzaj/img/img-icon/icon-projeyonetim.png" />
+          <MusicCardDemo
+            proje="YÖNETİM"
+            link="https://images.unsplash.com/photo-1595256858804-ad34bc0ce444?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=889&q=80"
+          />
+          <MusicCardDemo
+            proje="DANIŞMANLIK"
+            link="https://images.unsplash.com/flagged/photo-1552686234-8e47d5602508?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=726&q=80"
+          />
         </div>
       </div>
       <Footer />
