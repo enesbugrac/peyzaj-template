d995ffa20277ae502036ca8d853b6e53a84e1b63
enesbugrac <enesbc46@gmail.com>
2021-06-29 17:53:21 +0300

    ss

--- src/components/DetailsHeroAre.js
+++ src/components/DetailsHeroAre.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./DetailHero.css";
-import are from "../videos/are.mp4";
 import Fade from "react-reveal/Fade";
 function DetailsHeroAre() {
   return (
@@ -11,7 +10,12 @@ function DetailsHeroAre() {
         background: `-webkit-linear-gradient(bottom, rgba(39,39,39,1) 0%,rgba(39,39,39,0.75) 26%,rgba(39,39,39,0.2) 100%)`,
       }}
     >
-      <video src={are} autoPlay loop muted />
+      <video
+        src="https://firebasestorage.googleapis.com/v0/b/peyzaj-30d27.appspot.com/o/videos%2Fare.mp4?alt=media&token=0f9c2a71-58d4-479f-85bc-7672a28042f8"
+        autoPlay
+        loop
+        muted
+      />
       <br></br>
       <br></br>
       <br></br>
--- src/components/DetailsHeroFidan.js
+++ src/components/DetailsHeroFidan.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./DetailHero.css";
-import fidanlik from "../videos/fidan merkezi.mp4";
 import Fade from "react-reveal/Fade";
 function DetailsHeroAre() {
   return (
@@ -11,7 +10,12 @@ function DetailsHeroAre() {
         background: `-webkit-linear-gradient(bottom, rgba(39,39,39,1) 0%,rgba(39,39,39,0.75) 26%,rgba(39,39,39,0.2) 100%)`,
       }}
     >
-      <video src={fidanlik} autoPlay loop muted />
+      <video
+        src="https://firebasestorage.googleapis.com/v0/b/peyzaj-30d27.appspot.com/o/videos%2Ffidan%20merkezi.mp4?alt=media&token=9ba59875-b7ff-4072-9e55-c5bc17b2b64c"
+        autoPlay
+        loop
+        muted
+      />
       <br></br>
       <br></br>
       <br></br>
